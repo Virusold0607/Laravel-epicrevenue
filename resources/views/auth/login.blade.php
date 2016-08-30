5c4d6d4b7de131d664455c1b5cfc3341d944356b
Muhammad Abdullah <abdullahnaseer999@gmail.com>
2016-08-30 15:32:31 +0500

    Some design changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 @section('body')
     <div class="container page-container page-top-margin">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-6 col-md-offset-3">
                 <form class="form-horizontal form-custom" role="form" method="POST" action="{{ url('/login') }}">
                     {{ csrf_field() }}
 
