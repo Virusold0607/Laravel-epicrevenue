5c97a1c3eab70576cfd236626c2df598e600096b
Muhammad Abdullah <abdullahnaseer999@gmail.com>
2016-10-17 22:14:49 +0500

    Update

--- config/app.php
+++ config/app.php
@@ -183,7 +183,7 @@ return [
         Collective\Html\HtmlServiceProvider::class,
         Laravel\Socialite\SocialiteServiceProvider::class,
         Anhskohbo\NoCaptcha\NoCaptchaServiceProvider::class,
-        Torann\GeoIP\GeoIPServiceProvider::class,
+        \Torann\GeoIP\GeoIPServiceProvider::class,
         Jenssegers\Agent\AgentServiceProvider::class,
 
     ],
@@ -237,7 +237,7 @@ return [
         'Html'       => Collective\Html\HtmlFacade::class,
         'Socialite'  => Laravel\Socialite\Facades\Socialite::class,
         'Instagram'  => MetzWeb\Instagram\Instagram::class,
-        'GeoIP'      => Torann\GeoIP\GeoIPFacade::class,
+        'GeoIP'      => \Torann\GeoIP\Facades\GeoIP::class,
         'Agent'      => Jenssegers\Agent\Facades\Agent::class,
     ],
 
