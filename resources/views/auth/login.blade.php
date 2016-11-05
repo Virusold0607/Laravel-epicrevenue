769b773d7ddf745dc72882d68dea3e64e5eb6692
Muhammad Abdullah <abdullahnaseer999@gmail.com>
2016-11-05 21:52:50 +0500

    Login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,47 @@
 @extends('shared/layout')
 
 @section('body')
-    <div class="container page-container page-top-margin">
+    <div class="container" style="height: 20px;"></div>
+    <div class="container page-top-margin">
         <div class="row">
             <div class="col-md-6 col-md-offset-3">
-                <form class="form-horizontal form-custom" role="form" method="POST" action="{{ url('/login') }}">
-                    {{ csrf_field() }}
+                <div class="panel panel-default networks">
+                    <div class="panel-heading">Login</div>
+                    <div class="panel-body">
+                        <form class="" role="form" method="POST" action="{{ url('/login') }}">
+                            {{ csrf_field() }}
 
-                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="Email">
+                            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                                <input class="mdl-textfield__input" type="email" id="email" name="email" value="{{ old('email') }}">
+                                <label class="mdl-textfield__label" for="email">Email</label>
+                            
+                                @if ($errors->has('email'))
+                                    <span class="mdl-textfield__error">{{ $errors->first('email') }}</span>
+                                @endif
+                            </div>
 
-                        @if ($errors->has('email'))
-                            <span class="help-block">
-                                    <strong>{{ $errors->first('email') }}</strong>
-                                </span>
-                        @endif
-                    </div>
 
-                    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                        <input id="password" type="password" class="form-control" name="password" placeholder="Password">
+                            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                                <input class="mdl-textfield__input" type="password" id="password" name="password">
+                                <label class="mdl-textfield__label" for="password">Password</label>
+                            
+                                @if ($errors->has('password'))
+                                    <span class="mdl-textfield__error">{{ $errors->first('password') }}</span>
+                                @endif
+                            </div>
 
-                        @if ($errors->has('password'))
-                            <span class="help-block">
-                                <strong>{{ $errors->first('password') }}</strong>
-                            </span>
-                        @endif
-                    </div>
-
-                    <div class="form-group">
-                        <div class="checkbox">
-                            <label>
-                                <input type="checkbox" name="remember"> Remember Me
+                            <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="remember">
+                                <input type="checkbox" id="remember" class="mdl-checkbox__input" name="remember"  checked>
+                                <span class="mdl-checkbox__label">Remember Me</span>
                             </label>
-                        </div>
-                    </div>
 
-                    <div class="form-group">
-                        <button type="submit" class="btn btn-primary">
-                            <i class="fa fa-btn fa-sign-in"></i> Login
-                        </button>
-
-                        <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
+                            <!-- Accent-colored raised button with ripple -->
+                            <button type="submit" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--primary" data-upgraded=",MaterialButton,MaterialRipple">
+                            Login
+                            </button>
+                        </form>
                     </div>
-                </form>
+                </div>
             </div>
         </div>
     </div>
