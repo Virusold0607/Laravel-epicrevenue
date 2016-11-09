66848b94431d9482f8934f2a5fb3b3a03882737a
Muhammad Abdullah <abdullahnaseer999@gmail.com>
2016-11-09 17:20:31 +0500

    Login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,17 +1,27 @@
 @extends('shared/layout')
 
+
 @section('body')
     <div class="container page-top-margin" style="padding-top: 15px;">
         <div class="row">
             <div class="col-md-6 col-md-offset-3">
-                <form class="form-horizontal form-custom" role="form" method="POST" action="{{ url('/login') }}">
+                <form class="form-horizontal form-login" role="form" method="POST" action="{{ url('/login') }}">
                     {{ csrf_field() }}
 
                     <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Login</h3>
+                        </div>
+                        
                         <div class="panel-body">
                             <div class="container-fluid">
+
                                 <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="Email">
+                                    <label class="control-label"></label>
+                                    <div class="inner-addon left-addon">
+                                        <i class="glyphicon glyphicon-user"></i>
+                                        <input type="text" class="form-control" name="email" value="{{ old('email') }}" placeholder="Email" />
+                                    </div>
 
                                     @if ($errors->has('email'))
                                         <span class="help-block">
@@ -21,7 +31,11 @@
                                 </div>
 
                                 <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                                    <input id="password" type="password" class="form-control" name="password" placeholder="Password">
+                                    <label class="control-label"></label>
+                                    <div class="inner-addon left-addon">
+                                        <i class="glyphicon glyphicon-lock"></i>
+                                        <input id="password" type="password" class="form-control" name="password" placeholder="Password">
+                                    </div>
 
                                     @if ($errors->has('password'))
                                         <span class="help-block">
