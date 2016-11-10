33be901215d9bc6354db9ea8673d57f7d034e83e
Muhammad Abdullah <abdullahnaseer999@gmail.com>
2016-11-10 13:12:02 +0500

    Login page mobile design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,6 +2,66 @@
 
 
 @section('body')
+    @if($is_mobile)
+<div class="container page-top-margin" style="padding-top: 15px; margin-bottom: 50px;">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <form class="form-horizontal form-login" role="form" method="POST" action="{{ url('/login') }}">
+                    {{ csrf_field() }}
+                        <h2>Login</h2>
+                    
+                        <div class="container">
+
+                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                <label class="control-label"></label>
+                                <div class="inner-addon left-addon">
+                                    <i class="glyphicon glyphicon-user"></i>
+                                    <input type="text" class="form-control" name="email" value="{{ old('email') }}" placeholder="Email" />
+                                </div>
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                            <strong>{{ $errors->first('email') }}</strong>
+                                        </span>
+                                @endif
+                            </div>
+
+                            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                <label class="control-label"></label>
+                                <div class="inner-addon left-addon">
+                                    <i class="glyphicon glyphicon-lock"></i>
+                                    <input id="password" type="password" class="form-control" name="password" placeholder="Password">
+                                </div>
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+
+                        </div>
+                        <div class="container-fluid" style="height: 15px;"></div>
+                        <div class="text-left" style="display: inline-block;padding-top: 6px;">
+                            <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="remember">
+                                <input type="checkbox" id="remember" class="mdl-checkbox__input">
+                                <span class="mdl-checkbox__label">Remember Me</span>
+                            </label>
+                        </div>
+                        <div class="text-right" style="display: inline-block;position: absolute;right:0;">
+                            <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
+                        </div>
+                        <div class="container-fluid" style="height: 15px;"></div>
+                        
+                        <!-- Colored raised button -->
+                        <button type="submit" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" style="background-color: #3b76ed;width: 100%">
+                        Login
+                        </button>
+                </form>
+            </div>
+        </div>
+    </div>
+    @else
     <div class="container page-top-margin" style="padding-top: 15px; margin-bottom: 50px;">
         <div class="row">
             <div class="col-md-6 col-md-offset-3">
@@ -61,4 +121,5 @@
             </div>
         </div>
     </div>
+    @endif
 @endsection
