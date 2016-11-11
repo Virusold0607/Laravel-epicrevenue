70d834d64c9071c9ff32e977145248604277dc9d
Muhammad Abdullah <abdullahnaseer999@gmail.com>
2016-11-11 19:35:24 +0500

    Fixes

--- app/Http/Controllers/Admin/Api/AdminController.php
+++ app/Http/Controllers/Admin/Api/AdminController.php
@@ -18,25 +18,25 @@ class AdminController extends Controller
      */
     public function index()
     {
-        $data['today_clicks'] = Report::click()->today()->count();
+        $data['today_clicks'] = Report::today()->count();
         $data['today_leads'] = Report::lead()->today()->count();
         $data['earnings_today'] = Report::lead()->today()->sum('network_rate');
         $data['profits_today'] = Report::lead()->today()->sum('rate');
         $data['profits_today'] = $data['earnings_today'] - $data['profits_today'];
 
-        $data['yesterday_clicks'] = Report::click()->yesterday()->count();
+        $data['yesterday_clicks'] = Report::yesterday()->count();
         $data['yesterday_leads'] = Report::lead()->yesterday()->count();
         $data['earnings_yesterday'] = Report::lead()->yesterday()->sum('network_rate');
         $data['profits_yesterday'] = Report::lead()->yesterday()->sum('rate');
         $data['profits_yesterday'] = $data['earnings_yesterday'] - $data['profits_yesterday'];
 
-        $data['month_clicks'] = Report::click()->month()->count();
+        $data['month_clicks'] = Report::month()->count();
         $data['month_leads'] = Report::lead()->month()->count();
         $data['earnings_month'] = Report::lead()->month()->sum('network_rate');
         $data['profits_month'] = Report::lead()->month()->sum('rate');
         $data['profits_month'] = $data['earnings_month'] - $data['profits_month'];
 
-        $data['total_clicks'] = Report::click()->count();
+        $data['total_clicks'] = Report::count();
         $data['total_leads'] = Report::lead()->count();
         $data['earnings_total'] = Report::lead()->sum('network_rate');
         // Total profits
