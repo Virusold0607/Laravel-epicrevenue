a3eba5056758c9457e8d4b42a90fbb0af507cae2
Muhammad Adullah <abdullahnaseer999@gmail.com>
2017-05-06 23:36:15 +0500

    New Design Implementing

--- resources/views/home/influencers.blade.php
+++ resources/views/home/influencers.blade.php
@@ -1,61 +1,86 @@
 @extends('shared.layout')
 
 @section('body')
-    <div class="hero hero-influencers">
-        <div class="container">
-            <h1>INFLUENCERS</h1>
-            <br />
-            <div class="cameraman">
-                <img src="/images/cameraman.png" alt="">
+    <div class="main campaigns influencer">
+        <div class="home-banner campaigns-header">
+            <img src="/images/campaigns-header.png" class="img-responsive">
+            <div class="home-body">
+                <div class="container">
+                    <h1>INFLUENCERS</h1>
+                    <div class="influencer-header-graphic"><img src="/images/influencer1.png" class="img-responsive"></div>
+                </div>
             </div>
         </div>
-    </div><!-- End .hero -->
-    <div class="clearfix"></div>
 
-    <div class="maximize-earning influencers-page-content">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-7">
-                    <h1>Are you ready to maximize your earnings?</h1>
-                    <p class="trusted-text">Trusted by more than 10 million accounts</p>
+        <div class="influencer-panel">
+            <div class="influencer-header-graphic influencer-header-graphic2"><img src="/images/influencer2.png" class="img-responsive"></div>
+            <div class="influencer-maximize">
+                <div class="container">
+                    <h2 class="page-title-3">Are	you	ready	to	maximize  </br> your	revenue?</h2>
+                    <p>Trusted	by	more	than	10	million	accounts</p>
                 </div>
             </div>
-            <h2 class="text-center"><strong>FEATURES</strong></h2>
-            <div class="row text-center">
-                <div class="col-sm-4">
-                    <div class="ir-icon"><img class="img-responsive" src="/images/influencers/m.png" alt="The best network"></div>
-                    <h3>The best network</h3>
-                    <p class="font-light-gray">We offer what your favorite network offers and tons more. We have features they dont have, we have higher conversion rate, we pay you faster, and we offer top of the line support.</p>
-                </div>
-                <div class="col-sm-4">
-                    <div class="ir-icon"><img class="img-responsive" src="/images/influencers/n.png" alt="Advanced Security"></div>
-                    <h3>Advanced Security</h3>
-                    <p class="font-light-gray">Our network runs on an SSL secured connection meaning all information is transferred securely. Along with that your personal information is encrpyted with bank level encryption.</p>
-                </div>
-                <div class="col-sm-4">
-                    <div class="ir-icon"><img class="img-responsive" src="/images/influencers/h.png" alt="Optimization"></div>
-                    <h3>Features</h3>
-                    <p class="font-light-gray">Track your page growth and engagement, easily find influencers to do shoutouts with, these are some of the things you can do as an influencer.</p>
-                </div>
-                <div class="clearfix"></div>
-                <div class="col-sm-4">
-                    <div class="ir-icon"><img class="img-responsive" src="/images/influencers/p.png" alt="Optimization"></div>
-                    <h3>Optimization</h3>
-                    <p class="font-light-gray">We have a custom properiety system that automatically optimizate ALL traffic you send. Desktop and mobile traffic is intelligently sent to the best campaign.</p>
-                </div>
-                <div class="col-sm-4">
-                    <div class="ir-icon"><img class="img-responsive" src="/images/influencers/q.png" alt="Weekly Payments!"></div>
-                    <h3>Weekly Payments!</h3>
-                    <p class="font-light-gray">Every Friday if your balance to be paid is over your set threshold you will be paid. No need to request the payment it will automatically be sent to your selected payment method.</p>
-                </div>
-                <div class="col-sm-4">
-                    <div class="ir-icon"><img class="img-responsive" src="/images/influencers/r.png" alt="Creatives & Landing Pages"></div>
-                    <h3>Creatives & Landing Pages</h3>
-                    <p class="font-light-gray">Have an idea of something you want to promote? Let our team know and they create it for you. Setup a custom shop, page, etc.</p>
+            <div class="influencer-feature text-center">
+                <h3>FEATURES</h3>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <div class="feature-box">
+                                <div class="feature-box-image">
+                                    <img src="/images/influencer3.png" class="img-responsive">
+                                </div>
+                                <h4>THE BEST NETWORK</h4>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took </p>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="feature-box">
+                                <div class="feature-box-image">
+                                    <img src="/images/influencer4.png" class="img-responsive">
+                                </div>
+                                <h4>ADVANCED SECURITY</h4>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took </p>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="feature-box">
+                                <div class="feature-box-image">
+                                    <img src="/images/influencer5.png" class="img-responsive">
+                                </div>
+                                <h4>FEATURES</h4>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took </p>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="feature-box">
+                                <div class="feature-box-image">
+                                    <img src="/images/influencer6.png" class="img-responsive">
+                                </div>
+                                <h4>OPTIMIZATIONS</h4>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took </p>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="feature-box">
+                                <div class="feature-box-image">
+                                    <img src="images/influencer7.png" class="img-responsive">
+                                </div>
+                                <h4>WEEKLY PAYMENTS</h4>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took </p>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="feature-box">
+                                <div class="feature-box-image">
+                                    <img src="/images/influencer8.png" class="img-responsive">
+                                </div>
+                                <h4>CREATIVES & LANDING PAGES</h4>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took </p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="clearfix"></div>
-    <div class="container" style="height:30px;"></div>
 @endsection
