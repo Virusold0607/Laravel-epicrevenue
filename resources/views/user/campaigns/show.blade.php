223a9634887f3a3afb6ebc076fcda95c60514d16
Muhammad Adullah <abdullahnaseer999@gmail.com>
2017-05-24 23:12:09 +0500

    Design

--- resources/views/user/campaigns/show.blade.php
+++ resources/views/user/campaigns/show.blade.php
@@ -39,7 +39,7 @@
 
             @if(auth()->check())
                 <div class="">
-                    <button id="generateBtn" type="button" class="btn btn-primary btn-extra-padding" data-toggle="modal" data-target="#myModal">Generate Your Unique Promotional Link</button>
+                    <button id="generateBtn" type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Generate Your Unique Promotional Link</button>
                 </div>
             @endif
 
@@ -60,7 +60,7 @@
 
             <hr>
 
-            <div class="pull-right">
+            <div class="">
                 <a class="btn btn-primary btn-lg" href="{!! url('/campaigns')  !!}">Go Back</a>
             </div>
             <div class="clearfix"></div>
