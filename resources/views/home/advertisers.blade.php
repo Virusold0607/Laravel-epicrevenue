9da471d6789ddbc28527bf077fcbe7ae96126fb4
Muhammad Adullah <abdullahnaseer999@gmail.com>
2017-07-12 22:46:17 +0500

    Advertisers page

--- resources/views/home/advertisers.blade.php
+++ resources/views/home/advertisers.blade.php
@@ -7,4 +7,17 @@
             <h4 class="trusted">Trusted by more than <span class="green">< Total Publishers ></span> accounts</h4>
         </div>
     </div><!-- End .hero -->
+
+
+    <div class="main campaigns influencer">
+        <div class="home-banner campaigns-header">
+            <img src="/images/campaigns-header.png" class="img-responsive">
+            <div class="home-body">
+                <div class="container">
+                    <h1>Recieve Quality Traffic and Track Your Performance</h1>
+                    <h4 class="trusted">Trusted by more than <span class="green">< Total Publishers ></span> accounts</h4>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
