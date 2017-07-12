9f7a7a9323b93058db5c228cacac1edc678f267e
Muhammad Adullah <abdullahnaseer999@gmail.com>
2017-07-12 22:47:10 +0500

    Advertisers page

--- resources/views/home/advertisers.blade.php
+++ resources/views/home/advertisers.blade.php
@@ -1,21 +1,13 @@
 @extends('shared.layout')
 
 @section('body')
-    <div class="hero">
-        <div class="container">
-            <h1>Recieve Quality Traffic and Track Your Performance</h1>
-            <h4 class="trusted">Trusted by more than <span class="green">< Total Publishers ></span> accounts</h4>
-        </div>
-    </div><!-- End .hero -->
-
-
     <div class="main campaigns influencer">
         <div class="home-banner campaigns-header">
             <img src="/images/campaigns-header.png" class="img-responsive">
             <div class="home-body">
                 <div class="container">
                     <h1>Recieve Quality Traffic and Track Your Performance</h1>
-                    <h4 class="trusted">Trusted by more than <span class="green">< Total Publishers ></span> accounts</h4>
+                    <h4 class="trusted text-center">Trusted by more than <span class="green">< Total Publishers ></span> accounts</h4>
                 </div>
             </div>
         </div>
