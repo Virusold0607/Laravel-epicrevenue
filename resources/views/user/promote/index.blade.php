e385a886c4c19d7c14e3fc2bef7e4ba094c8b0f0
Muhammad Abdullah <abdullahnaseer999@gmail.com>
2018-01-06 21:22:48 +0500

    col change on promotions page

--- resources/views/user/promote/index.blade.php
+++ resources/views/user/promote/index.blade.php
@@ -16,7 +16,7 @@
     <div class="page-container no-border">
         <div class="container" style="margin-top: 20px;">
             <div class="row">
-                <div class="col-sm-3">
+                <div class="col-sm-4">
                     <a class="thumbnail text-center" href="{!! url('/promotions') !!}">
                         <i class="fa fa-user-secret" aria-hidden="true" style="font-size: 100px; padding: 10px;"></i>
                         {{--                        <img class="img-responsive" src="{!! $account->profile_picture !!}" alt="Add Account">--}}
@@ -26,7 +26,7 @@
                         </div>
                     </a>
                 </div>
-                <div class="col-sm-3">
+                <div class="col-sm-4">
                     <a class="thumbnail text-center" href="{!! url('/campaigns') !!}">
                         <i class="fa fa-area-chart" aria-hidden="true" style="font-size: 100px; padding: 10px;"></i>
                         {{--                        <img class="img-responsive" src="{!! $account->profile_picture !!}" alt="Add Account">--}}
