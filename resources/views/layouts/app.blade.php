a3d9d0dca32267579a887733a4f5efe7e516523c
Tevar Johnson <tevarjohnson@gmail.com>
2018-10-01 21:37:54 +0000

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Influencers Reach</title>
+    <title>Epic Revenue</title>
 
     <!-- Styles -->
     <link href="/css/app.css" rel="stylesheet">
