bfd255f5b72e06d0a7d6d318216ea64407b26c7a
Amirdzhon <amir.webking@gmail.com>
2018-10-20 13:48:59 +0000

    Update layout.blade.php

--- resources/views/shared/layout.blade.php
+++ resources/views/shared/layout.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>@if(isset($meta->title)){{ $meta->title }} |@endif Ads And Affiliates</title>
+    <title>@if(isset($meta->title)){{ $meta->title }} |@endif Epic Revenue</title>
     <meta http-equiv="Content-type" content="text/html; charset=utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
