8e6d0fb046f83d0765694ef44dd70e0259e5104f
Amirdzhon <amir.webking@gmail.com>
2018-10-21 04:04:37 +0800

    admin Panel Title

--- resources/views/admin/shared/angular.blade.php
+++ resources/views/admin/shared/angular.blade.php
@@ -2,7 +2,7 @@
 <html ng-app="admin">
 <head>
     <meta charset="utf-8" />
-    <title>Admin Panel</title>
+    <title>Epic Revenue</title>
     <meta http-equiv="Content-type" content="text/html; charset=utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
