d611da96f6ca41d4607f3f7b5bb7cf9726a3d01c
Tevar Johnson <tevarjohnson@gmail.com>
2018-10-24 21:39:32 +0000

    Update footer.blade.php

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-4 col-xs-12 hidden-sm hidden-xs">
-                <h3 class="footer-brand">Epic Revenue</h3>
+                <h3 class="footer-brand"><img class="android-logo-image" src="/images/logo1.png"></h3>
             </div>
             <div class="col-sm-8">
                 <div class="col-sm-4 col-xs-12">
