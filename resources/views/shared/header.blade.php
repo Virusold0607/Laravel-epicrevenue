fde6920d190660190671ed64b5adaf6261fd6202
Tevar Johnson <tevarjohnson@gmail.com>
2018-10-25 21:14:18 +0000

    Update header.blade.php

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -1,39 +1,43 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+<div class="header">
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a href="{{url('/')}}"><img src="/images/logo1.png" alt="logo" class="navbar-brand img-responsive"></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
 
-<div id="navbar" class="navbar-collapse  collapse">
-    @if(auth()->check())
-        <ul class="nav navbar-nav">
-            <li class="{{ request()->is('dashboard') ? 'active' : '' }}"><a href="{{url('/dashboard')}}">DASHBOARD</a></li>
-            <li class="{{ request()->is('promote') ? 'active' : '' }}"><a href="{{ url('/promote') }}">EARN</a></li>
-            {{--<li class="{{ request()->is('campaigns') ? 'active' : '' }}"><a href="{{ url('/campaigns') }}">Campaigns</a></li>--}}
-            <li class="{{ request()->is('rewards') ? 'active' : '' }}"><a href="{{ url('/rewards') }}">REWARDS</a></li>
-            <li class="{{ request()->is('reports') ? 'active' : '' }}"><a href="{{ url('/reports') }}">REPORTS</a></li>
-            <li class="{{ request()->is('contests') ? 'active' : '' }}"><a href="{{ url('/contests') }}">CONTESTS</a></li>
-            <li class="dropdown hidden-lg hidden-md hidden-sm">
-                <a class="dropdown-toggle" data-toggle="dropdown" href="#">{{ auth()->user()->firstname }} <span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                    <li><a href="{{ url('/settings') }}">Settings</a></li>
-                    <li><a href="{{ url('/invite') }}">Invite</a></li>
-                    <li><a href="{{ url('/logout') }}">Logout</a></li>
+        <div id="navbar" class="navbar-collapse  collapse">
+            @if(auth()->check())
+                <ul class="nav navbar-nav">
+                    <li class="{{ request()->is('dashboard') ? 'active' : '' }}"><a href="{{url('/dashboard')}}">DASHBOARD</a></li>
+                    <li class="{{ request()->is('promote') ? 'active' : '' }}"><a href="{{ url('/promote') }}">EARN</a></li>
+                    {{--<li class="{{ request()->is('campaigns') ? 'active' : '' }}"><a href="{{ url('/campaigns') }}">Campaigns</a></li>--}}
+                    <li class="{{ request()->is('rewards') ? 'active' : '' }}"><a href="{{ url('/rewards') }}">REWARDS</a></li>
+                    <li class="{{ request()->is('reports') ? 'active' : '' }}"><a href="{{ url('/reports') }}">REPORTS</a></li>
+                    <li class="{{ request()->is('contests') ? 'active' : '' }}"><a href="{{ url('/contests') }}">CONTESTS</a></li>
+                    <li class="dropdown hidden-lg hidden-md hidden-sm">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">{{ auth()->user()->firstname }} <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="{{ url('/settings') }}">Settings</a></li>
+                            <li><a href="{{ url('/invite') }}">Invite</a></li>
+                            <li><a href="{{ url('/logout') }}">Logout</a></li>
+                        </ul>
+                    </li>
                 </ul>
-            </li>
-        </ul>
-    @else
-    <ul class="nav navbar-nav">
-        <li class="{{ request()->is('/') ? 'active' : '' }}"><a href="{{url('/')}}">HOME</a></li>
-        <li class="{{ request()->is('affiliates') ? 'active' : '' }}"><a href="{{ url('/affiliates') }}">Affiliates</a></li>
-        <li class="{{ request()->is('advertisers') ? 'active' : '' }}"><a href="{{ url('/advertisers') }}">ADVERTISERS</a></li>
-        <li class="{{ request()->is('rewards') ? 'active' : '' }}"><a href="{{ url('/rewards') }}">REWARDS</a></li>
-        <li class="{{ request()->is('login') ? 'active' : '' }}"><a href="{{ url('/login') }}">SIGN IN</a></li>
-        <li class="{{ request()->is('affiliate/apply') ? 'active' : '' }}"><a href="{{ url('/affiliate/apply') }}">SIGN UP</a></li>
-    </ul>
-    @endif
+            @else
+            <ul class="nav navbar-nav">
+                <li class="{{ request()->is('/') ? 'active' : '' }}"><a href="{{url('/')}}">HOME</a></li>
+                <li class="{{ request()->is('affiliates') ? 'active' : '' }}"><a href="{{ url('/affiliates') }}">Affiliates</a></li>
+                <li class="{{ request()->is('advertisers') ? 'active' : '' }}"><a href="{{ url('/advertisers') }}">ADVERTISERS</a></li>
+                <li class="{{ request()->is('rewards') ? 'active' : '' }}"><a href="{{ url('/rewards') }}">REWARDS</a></li>
+                <li class="{{ request()->is('login') ? 'active' : '' }}"><a href="{{ url('/login') }}">SIGN IN</a></li>
+                <li class="{{ request()->is('affiliate/apply') ? 'active' : '' }}"><a href="{{ url('/affiliate/apply') }}">SIGN UP</a></li>
+            </ul>
+            @endif
+        </div>
+        </nav>
+    </div>
 </div>
-</nav>
 
 <div class="header">
     <div class="container">
