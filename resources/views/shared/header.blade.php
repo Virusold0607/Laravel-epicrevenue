529be2d904b6b7674dc23fd1a291708e4d827e4a
Tevar Johnson <tevarjohnson@gmail.com>
2018-10-25 21:44:11 +0000

    Update header.blade.php

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -1,14 +1,14 @@
 <div class="header">
     <div class="container">
         <nav class="navbar navbar-expand-lg ">
-          <a href="{{url('/')}}"><img src="/images/logo1.png" alt="logo" class="logo img-responsive"></a>
+          <a href="{{url('/')}}"><img src="/images/logo1.png" alt="logo" class="navbar-brand img-responsive"></a>
 
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
         </div>
-        <div id="navbar" class="navbar-collapse  collapse">
+        <div id="navbar" class="navbar-collapse collapse pull-right">
             @if(auth()->check())
-                <ul class="nav navbar-nav pull-right">
+                <ul class="nav navbar-nav">
                     <li class="{{ request()->is('dashboard') ? 'active' : '' }}"><a href="{{url('/dashboard')}}">DASHBOARD</a></li>
                     <li class="{{ request()->is('promote') ? 'active' : '' }}"><a href="{{ url('/promote') }}">EARN</a></li>
                     {{--<li class="{{ request()->is('campaigns') ? 'active' : '' }}"><a href="{{ url('/campaigns') }}">Campaigns</a></li>--}}
