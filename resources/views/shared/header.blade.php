320dcd16fffbfb47359aadc94f7e263754c5e3df
Tevar Johnson <tevarjohnson@gmail.com>
2018-10-25 21:31:30 +0000

    Update header.blade.php

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -1,7 +1,7 @@
 <div class="header">
     <div class="container">
         <nav class="navbar navbar-expand-lg ">
-          <a href="{{url('/')}}"><img src="/images/logo1.png" alt="logo" class="navbar-brand img-responsive"></a>
+          <a href="{{url('/')}}"><img src="/images/logo1.png" alt="logo" class="logo img-responsive"></a>
 
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
