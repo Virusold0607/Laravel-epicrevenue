6cbd1726ab0bb58f678bdf2a3263649c59e2956c
Tevar Johnson <tevarjohnson@gmail.com>
2018-10-25 21:22:02 +0000

    Update header.blade.php

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -6,7 +6,7 @@
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
         </div>
-        <div id="navbar" class="navbar-collapse  collapse">
+        <div id="navbar" class="navbar-collapse  collapse pull-right">
             @if(auth()->check())
                 <ul class="nav navbar-nav">
                     <li class="{{ request()->is('dashboard') ? 'active' : '' }}"><a href="{{url('/dashboard')}}">DASHBOARD</a></li>
