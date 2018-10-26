2321761224c967ccb285bed1ef0fbb7b41053147
Tevar Johnson <tevarjohnson@gmail.com>
2018-10-26 04:14:32 +0000

    Update settings.blade.php

--- resources/views/user/settings/settings.blade.php
+++ resources/views/user/settings/settings.blade.php
@@ -6,7 +6,7 @@
 			<h1 class="hero-heading">Settings</h1>
 		</div>
 	</div>
-	<div class="page-container background-gray no-shadow">
+	<div class="page-container background-gray no-shadow no-border">
 		<div class="container">
 			<div class="row">
 				<div class="col-sm-8">
