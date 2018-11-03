13b967ce4c1b94f8ac0e5b6dd05fb6bbbd11a068
Tevar Johnson <tevarjohnson@gmail.com>
2018-11-03 18:37:08 +0000

    Update footer.blade.php

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -1,11 +1,11 @@
 <div class="footer">
     <div class="container">
         <div class="row">
-            <div class="col-sm-4 col-xs-12">
+            <div class="col-sm-8 col-xs-12">
                 <img class="footer-logo-image" src="/images/footer-logo.png">
             </div>
             <div class="col-sm-8">
-                <div class="col-sm-4 col-xs-12">
+                <div class="col-sm-6 col-xs-12">
                     <ul class="border-dashed">
                         <li class="footertitle">Navigation</li>
                         <li>
@@ -14,15 +14,17 @@
                         <li>
                             <a href="{{ url('/faqs') }}">FAQ's</a>
                         </li>
+                        <!--
                         <li>
                             <a href="{{ url('/contact') }}">Contact</a>
                         </li>
+                        -->
                         <li>
                             <a href="{{ url('/login') }}">Sign In ?</a>
                         </li>
                     </ul>
                 </div>
-                <div class="col-sm-4 col-xs-12">
+                <div class="col-sm-6 col-xs-12">
                     <ul class="border-dashed">
                         <li class="footertitle">Legal</li>
                         <li>
@@ -33,6 +35,7 @@
                         </li>
                     </ul>
                 </div>
+                <!--
                 <div class="col-sm-4 col-xs-12">
                     <ul class="border-dashed">
                         <li class="footertitle">Legal</li>
@@ -44,6 +47,7 @@
                         </li>
                     </ul>
                 </div>
+                -->
             </div>
         </div>
 
