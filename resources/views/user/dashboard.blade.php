868fcc6b5a0041bf430e1b58a7fa87377676e192
Tevar Johnson <tevarjohnson@gmail.com>
2019-01-19 23:42:10 +0000

    Update dashboard.blade.php

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -140,6 +140,16 @@
                             beginAtZero:true
                         }
                     }]
+                },
+                legend: {
+                    display: false
+                },
+                tooltips: {
+                    callbacks: {
+                       label: function(tooltipItem) {
+                              return tooltipItem.yLabel;
+                       }
+                    }
                 }
             }
         });
