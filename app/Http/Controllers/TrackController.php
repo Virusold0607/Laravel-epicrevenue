7924180f8277e9b2c8bf903760ea2ee0e8682487
Tevar Johnson <tevarjohnson@gmail.com>
2019-01-29 23:29:13 +0000

    Update TrackController.php

--- app/Http/Controllers/TrackController.php
+++ app/Http/Controllers/TrackController.php
@@ -204,6 +204,16 @@ class TrackController extends Controller
                         <div class="spinner"></div>
                         <h3>If you are not redirected,<br/><br/><a href="'.url($redirect_url).'" class="ir-redirect">Click here</a></h3>
                         <meta http-equiv="refresh" content="2; url='.url($redirect_url).'">
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-37796498-35"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag(\'js\', new Date());
+
+  gtag(\'config\', \'UA-37796498-35\');
+</script>
+
                     </body>
                 </html>';
         }
@@ -224,6 +234,15 @@ class TrackController extends Controller
                             <div class="spinner"></div>
                             <h3>If you are not redirected,<br/><br/><a href="'.url($redirect_url).'" class="ir-redirect">Click here</a></h3>
                             <meta http-equiv="refresh" content="2; url='.url($redirect_url).'">
+                            <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-37796498-35"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag(\'js\', new Date());
+
+  gtag(\'config\', \'UA-37796498-35\');
+</script>
                         </body>
                     </html>';
         }
