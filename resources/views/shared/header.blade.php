72d7ce59671ae483fb635e7655a36c7921a4a1bb
GGM <tevarjohnson@gmail.com>
2022-07-14 22:46:19 +0000

    Update header.blade.php

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -1,10 +1,10 @@
 <header class="navbar navbar-expand-lg py-3">
     <nav class="container">
-        <a class="navbar-brand col-auto" href="{{ route('index') }}">
+        <a class="navbar-brand col-auto" href="{{ url('/') }}">
                 <!--
                 <img src="{{ asset('img/logo.png') }}" width="50" height="50" alt="logo">
                 <img class="logo" src="https://districtgurus.com/public/uploads/all/SC008HOLHmfOeB8E3SxNDONHI7nad1YJcmSl0ds9.png" data-src="https://districtgurus.com/public/uploads/all/SC008HOLHmfOeB8E3SxNDONHI7nad1YJcmSl0ds9.png" alt="District Gurus">-->
-                #JewelryCG
+                #EpicRevenue
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
