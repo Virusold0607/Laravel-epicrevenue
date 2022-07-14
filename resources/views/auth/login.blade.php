cc664ce34975b6eea3753425ffb1515887821357
GGM <tevarjohnson@gmail.com>
2022-07-14 23:39:20 +0000

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,7 +17,7 @@
                 <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                     <label class="fw-700 mb-2">Email Address</label>
                     @if ($errors->has('email'))
-                    <span class="text-danger fw-700 mb-2">{{ $errors->first('email') }}</span>
+                    <div class="text-danger fw-700 mb-2">{{ $errors->first('email') }}</div>
                     @endif
                     <input type="text" class="form-control" name="email" value="{{ old('email') }}" placeholder="" />
                 </div>
@@ -25,7 +25,7 @@
                 <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                     <label class="fw-700 mb-2">Password</label>
                     @if ($errors->has('password'))
-                    <span class="text-danger fw-700 mb-2"><strong>{{ $errors->first('password') }}</strong></span>
+                    <div class="text-danger fw-700 mb-2">{{ $errors->first('password') }}</div>
                     @endif
                     <input id="password" type="password" class="form-control" name="password" placeholder="">
                 </div>
