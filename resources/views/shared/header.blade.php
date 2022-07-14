470a82f02f8cdb6ed52804b9e66a5c8af885a1c8
GGM <tevarjohnson@gmail.com>
2022-07-14 22:21:45 +0000

    Update header.blade.php

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -1,14 +1,66 @@
-<div class="header">
-    <div class="container">
-        <nav class="navbar navbar-expand-lg ">
-          <a href="{{url('/')}}"><img src="/images/logo1.png" alt="logo" class="logo img-responsive"></a>
+<header class="navbar navbar-expand-lg py-3">
+    <nav class="container">
+        <a class="navbar-brand col-auto" href="{{ route('index') }}">
+                <!--
+                <img src="{{ asset('img/logo.png') }}" width="50" height="50" alt="logo">
+                <img class="logo" src="https://districtgurus.com/public/uploads/all/SC008HOLHmfOeB8E3SxNDONHI7nad1YJcmSl0ds9.png" data-src="https://districtgurus.com/public/uploads/all/SC008HOLHmfOeB8E3SxNDONHI7nad1YJcmSl0ds9.png" alt="District Gurus">-->
+                #JewelryCG
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+                @if(auth()->check())
+                    <li class="nav-item menu-area">
+                        <a class="nav-link" href="{{ route('index') }}">Home</a>
+                    </li>
+                    <li class="nav-item menu-area {{ request()->is('dashboard') ? 'active' : '' }}">
+                        <a class="nav-link" href="{{url('/dashboard')}}">Dashboard</a>
+                    </li>
+                    <li class="nav-item menu-area {{ request()->is('campaigns') ? 'active' : '' }}">
+                        <a class="nav-link" href="{{ url('/campaigns') }}">Campaigns</a>
+                    </li>
+                    <li class="nav-item menu-area {{ request()->is('reports') ? 'active' : '' }}">
+                        <a class="nav-link" href="{{ url('/reports') }}">Analytics</a>
+                    </li>
+                <!--<li class="nav-item menu-area {{ request()->is('contests') ? 'active' : '' }}"><a class="nav-link" href="{{ url('/contests') }}">CONTESTS</a></li>-->
+                    <li class="nav-item dropdown">
+                        <a class="nav-link active dropdown-toggle" aria-current="page" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" href="#">{{ auth()->user()->firstname }}</a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="{{ url('/settings') }}">Settings</a></li>
+                            <li><a class="dropdown-item" href="{{ url('/invite') }}">Invite</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="{{ url('/logout') }}">Logout</a></li>
+                        </ul>
+                    </li>
+                @else
+                    <li class="nav-item menu-area">
+                        <a class="nav-link" href="{{ route('index') }}">Home</a>
+                    </li>
+                    <li class="nav-item menu-area {{ request()->is('affiliates') ? 'active' : '' }}">
+                        <a href="{{ url('/affiliates') }}">Partners</a>
+                    </li>
+                    <li class="nav-item menu-area {{ request()->is('advertisers') ? 'active' : '' }}">
+                        <a href="{{ url('/advertisers') }}">Advertisers</a>
+                    </li>
+                    <li class="nav-item {{ request()->is('login') ? 'active' : '' }}">
+                        <a class="nav-link" href="{{ url('/login') }}">Login</a>  
+                    </li>
+                    <li class="nav-item {{ request()->is('affiliate/apply') ? 'active' : '' }}">
+                        <a class="nav-link" href="{{ url('/affiliate/apply') }}">Register</a>  
+                    </li>
 
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed pull-right" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"> 
-            <!--<span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> -->
-            <span class="navbar-toggler-icon nk-icon-burger"> <span class="nk-t-1"></span> <span class="nk-t-2"></span> <span class="nk-t-3"></span> </span>
-            </button>
+                @endif
+
+            </ul>
         </div>
+    </nav>
+</header>
+
+
+
+
         <div id="navbar" class="navbar-collapse collapse pull-right">
             @if(auth()->check())
                 <ul class="nav navbar-nav">
