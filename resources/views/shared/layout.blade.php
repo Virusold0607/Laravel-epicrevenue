e67f777da6e43c94ddd9e82f00caf0ae37d5edb2
GGM <tevarjohnson@gmail.com>
2022-07-14 21:50:02 +0000

    Update layout.blade.php

--- resources/views/shared/layout.blade.php
+++ resources/views/shared/layout.blade.php
@@ -40,22 +40,22 @@
 
         <link rel="stylesheet" href="{{url('/assets/css/main.css')}}">
         @yield('styles')
-
     </head>
     <body @if(isset($bodyid))id="{{ $bodyid }}"@endif>
 
-        <!-- Page Heading -->
-        @include('shared/header')
+    <!-- Page Heading -->
+    @include('shared/header')
 
-        <!-- Page Content -->
-        @yield('body')
+    <!-- Page Content -->
+    @yield('body')
 
-        <!-- Page Footer -->
-        @include('shared/footer')
+    <!-- Page Footer -->
+    @include('shared/footer')
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 
-        <script type="text/javascript" src="{{ url('/assets/js/main.js') }}"></script>
-        @yield('scripts')
+    <script type="text/javascript" src="{{ url('/assets/js/main.js') }}"></script>
+    @yield('scripts')
+
     </body>
 </html>
