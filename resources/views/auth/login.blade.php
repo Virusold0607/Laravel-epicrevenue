253c69556e6c09d14e6a5e17127c19580214b72c
GGM <tevarjohnson@gmail.com>
2022-07-14 23:22:30 +0000

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,51 +8,40 @@
         <div class="px-4 py-3 py-lg-4 card rounded">
             <form class="form-horizontal form-login" role="form" method="POST" action="{{ url('/login') }}">
                 {{ csrf_field() }}
-                <h2>Login</h2>
-                <div class="container-fluid">
-
-                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                <label class="control-label"></label>
-                <div class="inner-addon left-addon">
-                <i class="glyphicon glyphicon-user"></i>
-                <input type="text" class="form-control" name="email" value="{{ old('email') }}" placeholder="Email" />
+                <div class="text-center pt-4 pb-4">
+                    <h1 class="h4 fw-600">
+                        Login
+                    </h1>
                 </div>
 
-                @if ($errors->has('email'))
-                <span class="help-block">
-                <strong>{{ $errors->first('email') }}</strong>
-                </span>
-                @endif
+                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                    <div class="inner-addon left-addon">
+                        <i class="glyphicon glyphicon-user"></i>
+                        <input type="text" class="form-control" name="email" value="{{ old('email') }}" placeholder="Email" />
+                    </div>
+                    @if ($errors->has('email'))
+                    <span class="help-block"><strong>{{ $errors->first('email') }}</strong></span>
+                    @endif
                 </div>
 
                 <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                <label class="control-label"></label>
-                <div class="inner-addon left-addon">
-                <i class="glyphicon glyphicon-lock"></i>
-                <input id="password" type="password" class="form-control" name="password" placeholder="Password">
+                    <label class="control-label"></label>
+                    <div class="inner-addon left-addon">
+                        <i class="glyphicon glyphicon-lock"></i>
+                        <input id="password" type="password" class="form-control" name="password" placeholder="Password">
+                    </div>
+                    @if ($errors->has('password'))
+                    <span class="help-block"><strong>{{ $errors->first('password') }}</strong></span>
+                    @endif
                 </div>
-
-                @if ($errors->has('password'))
-                <span class="help-block">
-                <strong>{{ $errors->first('password') }}</strong>
-                </span>
-                @endif
-                </div>
-
                 <div class="form-group">
-                <div class="checkbox">
-                <label>
-                <input type="checkbox" name="remember"> Remember Me
-                </label>
+                    <div class="checkbox">
+                        <label><input type="checkbox" name="remember"> Remember Me</label>
+                    </div>
                 </div>
-                </div>
-                </div>
-                <div class="container-fluid">
                 <div class="form-group">
-                <button type="submit" class="btn btn-primary">Login</button>
-                <a class="link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
-                </div>
-                </div>
+                    <button type="submit" class="btn btn-primary">Login</button>
+                    <a class="link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
                 </div>
             </form>
         </div>
