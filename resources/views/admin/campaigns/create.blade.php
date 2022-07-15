f6a719a60298b5ded12aa8f36ad053dbb27c6294
GGM <tevarjohnson@gmail.com>
2022-07-15 17:07:02 +0000

    Update create.blade.php

--- resources/views/admin/campaigns/create.blade.php
+++ resources/views/admin/campaigns/create.blade.php
@@ -84,6 +84,60 @@
             </div>
             <!-- !card -->
 
+            <div class="card mb-4">
+                <div class="card-body">
+                    <div class="mb-2">
+                        <label for="name" class="w-100 mb-1 fw-700">Targeting:</label>
+                        <div id="entry1" class="clonedInput" style="display:none;">
+                            <div style="margin-bottom:10px;float: left;width: 100%;">
+                                <div class="col-md-2 alpha">
+                                    <select class="form-control" name="tar_country[]">
+                                        <option value="">Select Country</option>
+                                        @foreach($countries as $id => $name)
+                                            <option value="{{ $name }}">{{ $name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                <div class="col-md-2 alpha">
+                                    <select class="form-control" name="tar_os[]">
+                                        <option value="">Select OS</option>
+                                        <option value="AndroidOS">Android</option>
+                                        <option value="iOS">iOS</option>
+                                    </select>
+                                </div>
+                                <div class="col-md-2 alpha">
+                                    <select name="tar_device[]" class="form-control">
+                                        <option value>Select Device</option>
+                                        <option value="Desktop">Desktop</option>
+                                        <option value="Mobile">Mobile</option>
+                                        <option value="Tablet">Tablet</option>
+                                    </select>
+                                </div>
+                                <div class="col-md-1 alpha">
+                                    <input class="form-control" type="number" name="tar_rate[]" placeholder="Rate" value="" step='0.01' />
+                                </div>
+                                <div class="col-md-1 alpha">
+                                    <input class="form-control" type="number" name="tar_network_rate[]" placeholder="Netwrok Rate" value="" step='0.01' />
+                                </div>
+                                <div class="col-md-3 alpha">
+                                    <input class="form-control" type="text" name="tar_url[]" value="http://" />
+                                </div>
+                                <div class="col-md-1 alpha omega">
+                                    <select name="tar_active[]" class="form-control">
+                                        <option value="yes">Yes</option>
+                                        <option value="no">No</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div id="addDelButtons">
+                            <input type="button" class= "btn btn-default" id="btnAdd" value="Add rule"> <input type="button" class= "btn btn-default" id="btnDel" value="Remove rule above">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- !card -->
+
         </div>
         <!-- !col-md-8 -->
 
@@ -218,58 +272,6 @@
                     </td>
                 </tr>
  
-
-
-                <tr>
-                    <td valign="top">Targeting:</td>
-                    <td>
-                        <div id="entry1" class="clonedInput" style="display:none;">
-                            <div style="margin-bottom:10px;float: left;width: 100%;">
-                                <div class="col-md-2 alpha">
-                                    <select class="form-control" name="tar_country[]">
-                                        <option value="">Select Country</option>
-                                        @foreach($countries as $id => $name)
-                                            <option value="{{ $name }}">{{ $name }}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                                <div class="col-md-2 alpha">
-                                    <select class="form-control" name="tar_os[]">
-                                        <option value="">Select OS</option>
-                                        <option value="AndroidOS">Android</option>
-                                        <option value="iOS">iOS</option>
-                                    </select>
-                                </div>
-                                <div class="col-md-2 alpha">
-                                    <select name="tar_device[]" class="form-control">
-                                        <option value>Select Device</option>
-                                        <option value="Desktop">Desktop</option>
-                                        <option value="Mobile">Mobile</option>
-                                        <option value="Tablet">Tablet</option>
-                                    </select>
-                                </div>
-                                <div class="col-md-1 alpha">
-                                    <input class="form-control" type="number" name="tar_rate[]" placeholder="Rate" value="" step='0.01' />
-                                </div>
-                                <div class="col-md-1 alpha">
-                                    <input class="form-control" type="number" name="tar_network_rate[]" placeholder="Netwrok Rate" value="" step='0.01' />
-                                </div>
-                                <div class="col-md-3 alpha">
-                                    <input class="form-control" type="text" name="tar_url[]" value="http://" />
-                                </div>
-                                <div class="col-md-1 alpha omega">
-                                    <select name="tar_active[]" class="form-control">
-                                        <option value="yes">Yes</option>
-                                        <option value="no">No</option>
-                                    </select>
-                                </div>
-                            </div>
-                        </div>
-                        <div id="addDelButtons">
-                            <input type="button" class= "btn btn-default" id="btnAdd" value="Add rule"> <input type="button" class= "btn btn-default" id="btnDel" value="Remove rule above">
-                        </div>
-                    </td>
-                </tr>
 
 
                     <tr>
