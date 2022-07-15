9c5c518342a35284234813fc74e6bc5abe76e41a
GGM <tevarjohnson@gmail.com>
2022-07-15 21:55:46 +0000

    Update TrackController.php

--- app/Http/Controllers/TrackController.php
+++ app/Http/Controllers/TrackController.php
@@ -362,11 +362,13 @@ class TrackController extends Controller
         return false;
     }
 */
+    // Returns true if report total count is greater than cap
     public static function checkTotalCap(Campaign $campaign) {
-    return $campaign->reports()->where('status', 2)->count() >= $campaign->cap;
+        return $campaign->reports()->where('status', 2)->count() >= $campaign->cap;
     }
 
+    // Returns true if report daily count is greater than cap
     public static function checkDailyCap(Campaign $campaign) {
-    return $campaign->reports()->where('created_at', Carbon::today())->where('status', 2)->count() >= $campaign->daily_cap;
+        return $campaign->reports()->where('created_at', Carbon::today())->where('status', 2)->count() >= $campaign->daily_cap;
     }
 }
