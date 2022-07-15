30e0b55e17333e41f3d462f8fc775fbcebd5e909
GGM <tevarjohnson@gmail.com>
2022-07-15 01:51:48 +0000

    Update angular.blade.php

--- resources/views/admin/shared/angular.blade.php
+++ resources/views/admin/shared/angular.blade.php
@@ -16,7 +16,7 @@
 
     <!-- CSS Implementing Plugins -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('assets/vendor/bootstrap-icons/font/bootstrap-icons.css') }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.0/font/bootstrap-icons.css">
 
     <!-- CSS Front Template -->
     <link rel="stylesheet" href="{{ asset('assets/css/core.css') }}" data-hs-appearance="default"as="style">
