9d387c0b68a0074933f8d180d18fc941c085f582
GGM <tevarjohnson@gmail.com>
2022-07-15 00:07:54 +0000

    Update address.blade.php

--- resources/views/auth/address.blade.php
+++ resources/views/auth/address.blade.php
@@ -10,102 +10,84 @@
 
         </div>
     </div><!-- End .hero -->
-    <div class="clearfix"></div>
-    <div class="container" style="height:60px;"></div>
+<div class="container">
+    <!-- row -->
+    <div class="row">
+        <!-- col -->
+        <div class="col-lg-4 col-md-4 col-12 mx-auto">
+            <!-- card -->
+            <div class="px-4 py-3 py-lg-4 card rounded">
 
+            {!! Form::model($user, array('url' => '/affiliate/apply/address', 'method' => 'post', 'class' => 'col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2','style' => '')) !!}
+            <div class="panel panel-default networks">
+                <div class="panel-heading">Address</div>
+                <div class="panel-body">
+                    {{-- Was there an error? --}}
+                    @if (count($errors) > 0)
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $errorr)
+                                    <li>{{ $errorr }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+                    @if (isset($error))
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($error as $e)
+                                    <li>{!! $e !!}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
 
-    <div class="container">
-        <!--
-        <ul id='timeline'>
-            <span id='timeline2' style="width:60%"></span>
-            <li class='work'>
-                <input class='radio' id='work5' name='works' type='radio' checked>
-                <div class="relative">
-                    <span class='date checked'>Account Details</span>
-                    <span class='circle checked'>1</span>
-                </div>
-            </li>
-            <li class='work'>
-                <input class='radio' id='work4' name='works' type='radio'>
-                <div class="relative">
-                    <span class='date'>Address</span>
-                    <span class='circle'>2</span>
-                </div>
-            </li>
-            <li class='work'>
-                <input class='radio' id='work3' name='works' type='radio'>
-                <div class="relative">
-                    <span class='date'>Payment Method</span>
-                    <span class='circle'>3</span>
-                </div>
-            </li>
-        </ul>
-        -->
-        {!! Form::model($user, array('url' => '/affiliate/apply/address', 'method' => 'post', 'class' => 'col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2','style' => '')) !!}
-        <div class="panel panel-default networks">
-            <div class="panel-heading">Address</div>
-            <div class="panel-body">
-                {{-- Was there an error? --}}
-                @if (count($errors) > 0)
-                    <div class="alert alert-danger">
-                        <ul>
-                            @foreach ($errors->all() as $errorr)
-                                <li>{{ $errorr }}</li>
-                            @endforeach
-                        </ul>
+                    <div class="form-group">
+                        {!! Form::label('address1', 'Address 1', array()) !!}
+                        {!! Form::text('address1', null, array('class' => 'form-control')) !!}
                     </div>
-                @endif
-                @if (isset($error))
-                    <div class="alert alert-danger">
-                        <ul>
-                            @foreach ($error as $e)
-                                <li>{!! $e !!}</li>
-                            @endforeach
-                        </ul>
+                    <div class="form-group">
+                        {!! Form::label('address2', 'Address 2', array()) !!}
+                        {!! Form::text('address2', null, array('class' => 'form-control')) !!}
                     </div>
-                @endif
+                    <div class="form-group">
+                        {!! Form::label('city', 'City', array()) !!}
+                        {!! Form::text('city', null, array('class' => 'form-control')) !!}
+                    </div>
 
-                <div class="form-group">
-                    {!! Form::label('address1', 'Address 1', array()) !!}
-                    {!! Form::text('address1', null, array('class' => 'form-control')) !!}
-                </div>
-                <div class="form-group">
-                    {!! Form::label('address2', 'Address 2', array()) !!}
-                    {!! Form::text('address2', null, array('class' => 'form-control')) !!}
-                </div>
-                <div class="form-group">
-                    {!! Form::label('city', 'City', array()) !!}
-                    {!! Form::text('city', null, array('class' => 'form-control')) !!}
-                </div>
+                    <div class="form-group">
+                        {!! Form::label('state', 'State', array()) !!}
+                        {!! Form::text('state', null, array('class' => 'form-control')) !!}
+                    </div>
 
-                <div class="form-group">
-                    {!! Form::label('state', 'State', array()) !!}
-                    {!! Form::text('state', null, array('class' => 'form-control')) !!}
-                </div>
+                    <div class="form-group">
+                        {!! Form::label('zip', 'Zip Code', array()) !!}
+                        {!! Form::text('zip', null, array('class' => 'form-control')) !!}
+                    </div>
 
-                <div class="form-group">
-                    {!! Form::label('zip', 'Zip Code', array()) !!}
-                    {!! Form::text('zip', null, array('class' => 'form-control')) !!}
-                </div>
+                    <div class="form-group">
+                        {!! Form::label('phone', 'Phone Number', array()) !!}
+                        {!! Form::text('phone', null, array('class' => 'form-control')) !!}
+                    </div>
+                    <div class="form-group">
+                        {!! Form::label('whatsapp', 'Whatsapp Number', array()) !!}
+                        {!! Form::text('whatsapp', null, array('class' => 'form-control')) !!}
+                    </div>
+                    <div class="form-group">
+                        {!! Form::label('skype', 'Skype Id', array()) !!}
+                        {!! Form::text('skype', null, array('class' => 'form-control')) !!}
+                    </div>
 
-                <div class="form-group">
-                    {!! Form::label('phone', 'Phone Number', array()) !!}
-                    {!! Form::text('phone', null, array('class' => 'form-control')) !!}
+                    <br /><br />
                 </div>
-                <div class="form-group">
-                    {!! Form::label('whatsapp', 'Whatsapp Number', array()) !!}
-                    {!! Form::text('whatsapp', null, array('class' => 'form-control')) !!}
-                </div>
-                <div class="form-group">
-                    {!! Form::label('skype', 'Skype Id', array()) !!}
-                    {!! Form::text('skype', null, array('class' => 'form-control')) !!}
-                </div>
-
-                <br /><br />
             </div>
+            {!! Form::submit('Next', array('class' => 'btn btn-primary btn-lg pull-right')) !!}
+            {!! Form::close() !!}
+            </div>
+            <!-- !card -->
         </div>
-        {!! Form::submit('Next', array('class' => 'btn btn-primary btn-lg pull-right')) !!}
-        {!! Form::close() !!}
-
+        <!-- !col -->
     </div>
+    <!-- !row -->
+</div>
 @endsection
