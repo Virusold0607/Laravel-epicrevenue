59afd95fc79b98ea209788404cdd412f12b19894
GGM <tevarjohnson@gmail.com>
2022-07-15 00:59:39 +0000

    Update angular.blade.php

--- resources/views/admin/shared/angular.blade.php
+++ resources/views/admin/shared/angular.blade.php
@@ -46,11 +46,12 @@
 </footer>
 
 <!-- JS Global Compulsory  -->
+<!--
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-migrate/3.4.0/jquery-migrate.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
 
-
+-->
 <!-- JS Front -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/js/select2.full.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.1.0/trumbowyg.min.js"></script>
