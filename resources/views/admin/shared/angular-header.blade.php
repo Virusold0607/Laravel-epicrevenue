2141153907aa2eacf96b4c105546e9e6c2e10298
GGM <tevarjohnson@gmail.com>
2022-07-15 03:37:15 +0000

    Update angular-header.blade.php

--- resources/views/admin/shared/angular-header.blade.php
+++ resources/views/admin/shared/angular-header.blade.php
@@ -4,7 +4,7 @@
         <a class="navbar-brand col-auto" href="https://jewelrycadfiles.com/backend">
             #EpicRevenue
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <!--<span class="navbar-toggler-icon"></span>-->
         <i class="bi bi-list nav-icon"></i>
         </button>
