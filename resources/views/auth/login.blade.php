d27a21dbdef821e83fc4f4172934089273618be3
GGM <tevarjohnson@gmail.com>
2022-07-15 00:05:55 +0000

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
     <!-- row -->
     <div class="row">
         <!-- col -->
-        <div class="col-xxl-4 col-xl-5 col-lg-6 col-md-8 mx-auto">
+        <div class="col-lg-4 col-md-4 col-12 mx-auto">
             <!-- card -->
             <div class="px-4 py-3 py-lg-4 card rounded">
                 <form class="form-horizontal form-login" role="form" method="POST" action="{{ url('/login') }}">
