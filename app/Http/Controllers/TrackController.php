c7c127287761c9e597d88e2be7ea047db3027a68
GGM <tevarjohnson@gmail.com>
2022-07-15 15:10:33 +0000

    Update TrackController.php

--- app/Http/Controllers/TrackController.php
+++ app/Http/Controllers/TrackController.php
@@ -340,7 +340,7 @@ class TrackController extends Controller
     {
         foreach($campaign_countries as $allowed_country)
         {
-            if($allowed_country->short_name == $geoIP->country)
+            if($allowed_country->name == $geoIP->country)
                 return true;
         }
         return false;
