080273350cd47943d7249dfd797a326d1cd1cad2
GGM <tevarjohnson@gmail.com>
2022-07-15 00:40:40 +0000

    Update angular.blade.php

--- resources/views/admin/shared/angular.blade.php
+++ resources/views/admin/shared/angular.blade.php
@@ -1,14 +1,35 @@
 <!DOCTYPE html>
-<html ng-app="admin">
+<html lang="en">
+
 <head>
-    <meta charset="utf-8" />
+    <!-- Required Meta Tags Always Come First -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Title -->
     <title>Epic Revenue</title>
-    <meta http-equiv="Content-type" content="text/html; charset=utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="./favicon.ico">
+
+    <!-- Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet">
+
+    <!-- CSS Implementing Plugins -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/bootstrap-icons/font/bootstrap-icons.css') }}">
+
+    <!-- CSS Front Template -->
+    <link rel="stylesheet" href="{{ asset('assets/css/core.css') }}" data-hs-appearance="default"as="style">
+    <link rel="stylesheet" href="{{ asset('assets/css/admin/app.css') }}" data-hs-appearance="default"as="style">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.1.0/ui/trumbowyg.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta3/dist/css/bootstrap-select.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/css/select2.min.css" />
     @yield('styles')
     <link rel="stylesheet" href="{{ url('/assets/css/admin.css') }}">
 </head>
+
 <body>
 @include('admin.shared.angular-header')
 <div class="container">
