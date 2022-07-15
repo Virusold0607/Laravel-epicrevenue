fb9b33af68ac7a7c9e47352bc546931eb4d10e11
GGM <tevarjohnson@gmail.com>
2022-07-15 00:38:58 +0000

    Update angular-header.blade.php

--- resources/views/admin/shared/angular-header.blade.php
+++ resources/views/admin/shared/angular-header.blade.php
@@ -1,3 +1,38 @@
+<!-- ========== HEADER ========== -->
+<header class="navbar navbar-expand-lg py-3">
+    <nav class="container">
+        <a class="navbar-brand col-auto" href="https://jewelrycadfiles.com/backend">
+            #EpicRevenue
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <!--<span class="navbar-toggler-icon"></span>-->
+        <i class="bi bi-list nav-icon"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+                <li class="nav-item dropdown menu-area">
+                    <a class="nav-link active" href="https://jewelrycadfiles.com">Home</a>
+                </li>
+                <li class="nav-item dropdown menu-area">
+                    <a class="nav-link" href="https://jewelrycadfiles.com/3d-models">3D Models</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" aria-current="page" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" href="#">Learn</a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="https://jewelrycadfiles.com/blog">Blog</a></li>
+                        <li><a class="dropdown-item" href="https://jewelrycadfiles.com/blog/category/list/all">Categories</a></li>
+                        <li><a class="dropdown-item" href="https://jewelrycadfiles.com/blog/tag/list/all">Tags</a></li>
+                        <li><a class="dropdown-item" href="#">Comments</a></li>
+                    </ul>
+                </li>
+                
+            </ul>
+        </div>
+    </nav>
+</header>
+<!-- ========== END HEADER ========== -->
+
+
 <nav class="navbar navbar-inverse" role="navigation">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
