85557802f0540664fa3a34b531c1b1fbf12e33c4
GGM <tevarjohnson@gmail.com>
2022-07-16 01:09:28 +0000

    Update index.blade.php

--- resources/views/admin/campaigns/index.blade.php
+++ resources/views/admin/campaigns/index.blade.php
@@ -70,7 +70,9 @@ Below are all campaigns added to the network.<br /><br />
                     @if($leads + $unique_clicks <= 0)
                         0
                     @else
-                        {!! number_format($leads / ($leads + $unique_clicks), 2) !!}
+                        {!! number_format($leads / ($leads + $unique_clicks), 2) !!} 
+                        <br>
+                        {!! number_format($leads / ($leads + $clicks)) * 100), 2) !!}
                     @endif
                     %
                 </td>
