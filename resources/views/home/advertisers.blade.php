e64dbce1caec50a91fb14951e26ddeca9368eac9
GGM <tevarjohnson@gmail.com>
2022-07-20 02:30:33 +0000

    Update advertisers.blade.php

--- resources/views/home/advertisers.blade.php
+++ resources/views/home/advertisers.blade.php
@@ -1,10 +1,15 @@
 @extends('shared.layout')
 
 @section('body')
-    <div class="hero py-6">
-        <div class="container">
-            <h1>Recieve Quality Traffic and Track Your Performance</h1>
-            <h4 class="trusted">Trusted by more than <span class="green">< Total Publishers ></span> accounts</h4>
-        </div>
-    </div><!-- End .hero -->
+<div class="hero py-6">
+    <div class="container">
+        <h1 class="fw-800">Recieve Quality Traffic and Track Your Performance</h1>
+        <h4 class="trusted">Trusted by more than <span class="green">< Total Publishers ></span> accounts</h4>
+    </div>
+</div><!-- End .hero -->
+
+<div class="py-6">
+    <div class="container">
+    </div>
+</div><!-- End .hero -->
 @endsection
