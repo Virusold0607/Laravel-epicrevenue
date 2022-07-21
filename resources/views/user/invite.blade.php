9c2611f244eb48f80d0747828b70387f6d3571e5
GGM <tevarjohnson@gmail.com>
2022-07-21 16:18:01 +0000

    Update invite.blade.php

--- resources/views/user/invite.blade.php
+++ resources/views/user/invite.blade.php
@@ -42,7 +42,7 @@
     </div>
 </div>
 
-<div class="page-container no-shadow py-6">
+<div class="page-container py-6">
     <div class="container">
         <p>For every user you refer you earn 5% off every lead they get, this is a real great way to get your friends or others invloved while earning money when they join. Below you can find your referral link and a list of all the users you refered.</p>
 
@@ -50,32 +50,34 @@
             <h3>Your referral link: </h3>
             <input class="form-control" value="{!! url('/invite/' . auth()->user()->id) !!}" style="width: 600px; " />
         </div>
-        <div class="table-responsive">
-            <table class="table table-striped table-bordered table-hover">
-                <thead>
-                <tr>
-                    <th>ID</th>
-                    <!--<th>Email</th>-->
-                    <th>Date Joined</th>
-                </tr>
-                </thead>
-                <tbody>
-                @if(count($referrals) >0)
-                    @foreach($referrals as $r)
-                        <tr>
-                            <td>{!! $r->id !!} </td>
-                            <!--<td>'.$row->email_address.'</td>-->
-                            <td>{!! $r->created_at !!}</td>
-                        </tr>
-                    @endforeach
-                @else
+        <div class="card">
+            <div class="card-body">
+                <table class="table table-striped table-bordered table-hover table-responsive">
+                    <thead>
                     <tr>
-                        <td colspan="2">You have no referrals.</td>
+                        <th>ID</th>
+                        <!--<th>Email</th>-->
+                        <th>Date Joined</th>
                     </tr>
-                @endif
-                </tbody>
-            </table>
-        </div><!-- end .table-responsive -->
+                    </thead>
+                    <tbody>
+                    @if(count($referrals) >0)
+                        @foreach($referrals as $r)
+                            <tr>
+                                <td>{!! $r->id !!} </td>
+                                <!--<td>'.$row->email_address.'</td>-->
+                                <td>{!! $r->created_at !!}</td>
+                            </tr>
+                        @endforeach
+                    @else
+                        <tr>
+                            <td colspan="2">You have no referrals.</td>
+                        </tr>
+                    @endif
+                    </tbody>
+                </table>
+            </div>
+        </div><!-- end .card -->
 
     </div>
 </div>
