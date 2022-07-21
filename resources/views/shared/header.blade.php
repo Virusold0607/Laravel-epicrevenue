87c93c3eb9019f055df4af4bf4141956e180f9f8
GGM <tevarjohnson@gmail.com>
2022-07-21 00:14:21 +0000

    Update header.blade.php

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -7,7 +7,7 @@
                 #EPICREVENUE
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+        <i class="bi bi-list"></i>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
