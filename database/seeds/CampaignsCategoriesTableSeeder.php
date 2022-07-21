6e2a859d985af98e10a42bd995febeccdef3b9d1
GGM <tevarjohnson@gmail.com>
2022-07-21 17:44:26 +0000

    Update CampaignsCategoriesTableSeeder.php

--- database/seeds/CampaignsCategoriesTableSeeder.php
+++ database/seeds/CampaignsCategoriesTableSeeder.php
@@ -13,22 +13,6 @@ class CampaignsCategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-//        $data = array(
-//            'Actor' , 'Adult' , 'Animals' , 'Art & Design' , 'Beauty' , 'Black & White' , 'Blogger/Editor' , 'Car' , 'Celebrity' , 'Collage' , 'College Student' , 'Colorful' , 'Comedy' , 'Contacts' , 'Contemporary' , 'DIY' , 'DJ' , 'Entertainment' , 'Entrepreneur' , 'Facial Hair' , 'Family' , 'Fashion' , 'Finance' , 'Fitness' , 'Flat Lay' ,
-//            'Food' , 'Gaming' , 'Glasses' , 'Graffiti' , 'Health' , 'Illustrations/Graphics' , 'Interior' , 'LGBT' , 'Lifestyle' , 'Magic' , 'Married' , 'Model' , 'Motorcycle' , 'Music' , 'Musician' , 'Nightlife' , 'Photographer' , 'Photography' , 'Poker' , 'Repost Account' , 'Restaurateur/Catering' , 'Sommelier' , 'Sport' , 'Stop Motion' ,
-//            'Stylist/Designer' , 'Tech' , 'Teen' , 'Travel' , 'Viner' , 'Wedding' , 'Youtuber' , 'Alternative' , 'Anime' , 'Bohemian' , 'High Fashion' , 'Jewelry' , 'Mens Fashion' , 'Plus Size' , 'Sexy' , 'Streetwear' , 'Swimwear' , 'Vintage' , 'Watches' , 'Architecture' , 'Beach' , 'City' , 'Outdoors' , 'People' ,
-//            'Winter' , 'Baseball' , 'Basketball' , 'BMX' , 'Bowling' , 'Boxing' , 'Cheerleading' , 'Combat Sports' , 'Crossfit' , 'Cycling' , 'Dance' , 'Figure Skating' , 'Fishing' , 'Football' , 'Free Running/Parkour' , 'Golf' , 'Gymnastics' , 'Hockey' , 'Hunting' , 'Ice Skating' , 'Lacrosse' , 'MMA' , 'Motocross' , 'Polo' , 'Racing' ,
-//            'Rock Climbing' , 'Rugby' , 'Skateboarding' , 'Skiing' , 'Skydiving' , 'Snowboarding' , 'Snowmobiling' , 'Soccer' , 'Surfing' , 'Swimming' , 'Tennis' , 'Track & Field' , 'Volleyball' , 'Weightlifting' , 'Wrestling' , 'Yoga/Pilates' , 'Cat' , 'Dog' , 'Wild Animals' , 'Dad' , 'Kids' , 'Mom' , 'Dessert' , 'Gourmet' , 'Vegetarian' ,
-//            'EDM/Raver' , 'Guitar' , 'Hip Hop' , 'Piano' , 'Pop' , 'Hair' , 'Make Up' , 'Nails' , 'Tattoo'
-//        );
-//
-//        foreach($data as $c)
-//        {
-//            $cc = new CampaignsCategory();
-//            $cc->name = $c;
-//            $cc->save();
-//        }
-
 
         $cc = CampaignsCategory::firstOrNew(['name' => 'Adult']);
         $cc->save();
@@ -62,8 +46,6 @@ class CampaignsCategoriesTableSeeder extends Seeder
 
         $cc = CampaignsCategory::firstOrNew(['name' => 'Trials']);
         $cc->save();
-
-
 
         $p = PromotionCategory::firstOrNew(['name' => 'Sweepstakes/Giveaways']);
         $p->save();
