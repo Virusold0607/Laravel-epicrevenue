c7b7b870b2b32152e4cd140575ca8b1830d68bdd
GGM <tevarjohnson@gmail.com>
2022-07-21 14:09:19 +0000

    Update index.blade.php

--- resources/views/user/campaigns/index.blade.php
+++ resources/views/user/campaigns/index.blade.php
@@ -107,8 +107,8 @@
                                             <div class="col-lg-9">
                                             <a href="{{ url('/campaign/' . $campaign->id) }}">
                                                 <h3 class="text-black py-2 mb-0 fs-18">{{ $campaign->name }}</h3>
-                                                <span class="text-black opacity-50">Category: {{ $campaign->category->name }}</span>
-                                                <span class="text-black opacity-50">AVG. EPC: $0.44</span>
+                                                <div class="text-black opacity-50 mb-2">Category: {{ $campaign->category->name }}</div>
+                                                <div class="text-black opacity-50">AVG. EPC: $0.44</div>
                                                 <p class="py-2 mb-0 text-black d-none">{{ $campaign->description }}</p>
                                             </a>
                                             </div>
