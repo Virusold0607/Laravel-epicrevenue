c3a2e2584f5509cbb6a27aaff1ddf2b0d028fd42
GGM <tevarjohnson@gmail.com>
2022-07-21 15:50:07 +0000

    Update settings.blade.php

--- resources/views/user/settings/settings.blade.php
+++ resources/views/user/settings/settings.blade.php
@@ -11,92 +11,94 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-8 mx-auto">
-				<div class="col-sm-8">
-					<div class="card panel-default">
-						<div class="card-header">Account Information</div>
-						<div class="card-body">
-							@if (count($errors) > 0)
-								<div class="alert alert-danger">
-									@foreach ($errors->all() as $error)
-										<div class="alert alert-danger" role="alert">
-											<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-											{{ $error }}
-										</div>
-									@endforeach
+				<div class="row">
+					<div class="col-sm-8">
+						<div class="card panel-default">
+							<div class="card-header">Account Information</div>
+							<div class="card-body">
+								@if (count($errors) > 0)
+									<div class="alert alert-danger">
+										@foreach ($errors->all() as $error)
+											<div class="alert alert-danger" role="alert">
+												<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+												{{ $error }}
+											</div>
+										@endforeach
+									</div>
+								@endif
+								{!! Form::model($user, array('url' => '/settings/updateInfo', 'method' => 'post', 'class' => 'form-horizontal'))!!}
+								<div class="form-group">
+									{!! Form::label('firstname', 'First name', array('class' => 'col-sm-2 control-label')) !!}
+									{!! Form::text('firstname', null, array('class' => 'form-control', 'placeholder' => 'First name', 'required'=>'required', 'size'=>'70' )) !!}
 								</div>
-							@endif
-							{!! Form::model($user, array('url' => '/settings/updateInfo', 'method' => 'post', 'class' => 'form-horizontal'))!!}
-							<div class="form-group">
-								{!! Form::label('firstname', 'First name', array('class' => 'col-sm-2 control-label')) !!}
-								{!! Form::text('firstname', null, array('class' => 'form-control', 'placeholder' => 'First name', 'required'=>'required', 'size'=>'70' )) !!}
-							</div>
-							<div class="form-group">
-								{!! Form::label('lastname', 'Last name', array('class' => 'col-sm-2 control-label')) !!}
-								{!! Form::text('lastname', null, array('class' => 'form-control', 'placeholder' => 'Last name', 'cols' => '30', 'rows' => '6' )) !!}
-							</div>
-							<div class="form-group">
-								{!! Form::label('email', 'Email', array('class' => 'col-sm-2 control-label')) !!}
-								{!! Form::email('email', null, array('class' => 'form-control', 'disabled' )) !!}
-							</div>
-							<div class="form-group">
-								{!! Form::label('address1', 'Address', array('class' => 'col-sm-2 control-label')) !!}
-								{!! Form::text('address1', null, array('class' => 'form-control', 'placeholder' => 'Address line 1', 'required'=>'required', 'size'=>'70' )) !!}
-							</div>
-							<div class="form-group">
-								{!! Form::label('address2', '', array('class' => 'col-sm-2 control-label')) !!}
-								{!! Form::text('address2', null, array('class' => 'form-control', 'placeholder' => 'Address line 2', 'required'=>'required' )) !!}
-							</div>
-							<div class="form-group">
-								{!! Form::label('city', 'City', array('class' => 'col-sm-2 control-label')) !!}
-								{!! Form::text('city', null, array('class' => 'form-control', 'placeholder' => 'City', 'required'=>'required' )) !!}
-							</div>
-							<div class="form-group">
-								{!! Form::label('state', 'State', array('class' => 'col-sm-2 control-label')) !!}
-								{!! Form::text('state', null, array('class' => 'form-control', 'placeholder' => 'State')) !!}
-							</div>
-							<div class="form-group">
-								{!! Form::label('zip', 'Zipcode', array('class' => 'col-sm-2 control-label')) !!}
-								{!! Form::text('zip', null, array('class' => 'form-control', 'placeholder' => 'Zipcode')) !!}
-							</div>
+								<div class="form-group">
+									{!! Form::label('lastname', 'Last name', array('class' => 'col-sm-2 control-label')) !!}
+									{!! Form::text('lastname', null, array('class' => 'form-control', 'placeholder' => 'Last name', 'cols' => '30', 'rows' => '6' )) !!}
+								</div>
+								<div class="form-group">
+									{!! Form::label('email', 'Email', array('class' => 'col-sm-2 control-label')) !!}
+									{!! Form::email('email', null, array('class' => 'form-control', 'disabled' )) !!}
+								</div>
+								<div class="form-group">
+									{!! Form::label('address1', 'Address', array('class' => 'col-sm-2 control-label')) !!}
+									{!! Form::text('address1', null, array('class' => 'form-control', 'placeholder' => 'Address line 1', 'required'=>'required', 'size'=>'70' )) !!}
+								</div>
+								<div class="form-group">
+									{!! Form::label('address2', '', array('class' => 'col-sm-2 control-label')) !!}
+									{!! Form::text('address2', null, array('class' => 'form-control', 'placeholder' => 'Address line 2', 'required'=>'required' )) !!}
+								</div>
+								<div class="form-group">
+									{!! Form::label('city', 'City', array('class' => 'col-sm-2 control-label')) !!}
+									{!! Form::text('city', null, array('class' => 'form-control', 'placeholder' => 'City', 'required'=>'required' )) !!}
+								</div>
+								<div class="form-group">
+									{!! Form::label('state', 'State', array('class' => 'col-sm-2 control-label')) !!}
+									{!! Form::text('state', null, array('class' => 'form-control', 'placeholder' => 'State')) !!}
+								</div>
+								<div class="form-group">
+									{!! Form::label('zip', 'Zipcode', array('class' => 'col-sm-2 control-label')) !!}
+									{!! Form::text('zip', null, array('class' => 'form-control', 'placeholder' => 'Zipcode')) !!}
+								</div>
 
-							<div class="form-group">
-								<label for="name" class="col-sm-2 control-label"></label>
-								{!! Form::submit('Update Info', array('class' => 'btn btn-default')) !!}
+								<div class="form-group">
+									<label for="name" class="col-sm-2 control-label"></label>
+									{!! Form::submit('Update Info', array('class' => 'btn btn-default')) !!}
+								</div>
+								{!! Form::close() !!}
 							</div>
-							{!! Form::close() !!}
 						</div>
-					</div>
-				</div><!--end grid_4-->
+					</div><!--end grid_4-->
 
-				<div class="col-sm-4">
-					<div class="card">
-						<div class="card-header">Email Notifications</div>
-						<div class="card-body">
-							{!! Form::open(array('url' => '/settings/updateNotifications', 'method' => 'post'))!!}
-							<div class="form-group">
-								<div class="checkbox">
-									<label>
-										<input type="checkbox" name="newsletters" @if($user->emailNotification->newsletters === 'yes') checked @endif> Newsletter & Emails from Support
-									</label>
+					<div class="col-sm-4">
+						<div class="card">
+							<div class="card-header">Email Notifications</div>
+							<div class="card-body">
+								{!! Form::open(array('url' => '/settings/updateNotifications', 'method' => 'post'))!!}
+								<div class="form-group">
+									<div class="checkbox">
+										<label>
+											<input type="checkbox" name="newsletters" @if($user->emailNotification->newsletters === 'yes') checked @endif> Newsletter & Emails from Support
+										</label>
+									</div>
+									<div class="checkbox">
+										<label>
+											<input type="checkbox" name="leads" @if($user->emailNotification->leads === 'yes') checked @endif> New lead email
+										</label>
+									</div>
 								</div>
-								<div class="checkbox">
-									<label>
-										<input type="checkbox" name="leads" @if($user->emailNotification->leads === 'yes') checked @endif> New lead email
-									</label>
-								</div>
+								{!! Form::submit('Update Notifications', array('class' => 'btn btn-default')) !!}
+								{!! Form::close() !!}
 							</div>
-							{!! Form::submit('Update Notifications', array('class' => 'btn btn-default')) !!}
-							{!! Form::close() !!}
 						</div>
-					</div>
-					<div class="panel panel-default">
-						<div class="panel-body">
-							<a href="{!! url('/settings/password') !!}">Click here to change password</a>
+						<div class="panel panel-default">
+							<div class="panel-body">
+								<a href="{!! url('/settings/password') !!}">Click here to change password</a>
+							</div>
 						</div>
-					</div>
-					<div class="panel panel-default">
-						<div class="panel-body">
-							<a href="{!! url('/taxdetails') !!}">Click here to submit or update tax information</a>
+						<div class="panel panel-default">
+							<div class="panel-body">
+								<a href="{!! url('/taxdetails') !!}">Click here to submit or update tax information</a>
+							</div>
 						</div>
 					</div>
 				</div>
