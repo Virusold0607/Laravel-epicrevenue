74782830c134a6c7c02cd787372825a18f54664b
Tevar Johnson <tevarjohnson@gmail.com>
2022-07-22 08:18:04 +0000

    update

---
+++ config/shopify.php
@@ -0,0 +1,51 @@
+<?php
+
+return [
+
+    'credentials' => [
+
+        /*
+         * The API key from private app credentials.
+         */
+        'api_key' => env('SHOPIFY_API_KEY', ''),
+
+        /*
+         * The password from private app credentials.
+         */
+        'password' => env('SHOPIFY_PASSWORD', ''),
+
+        /*
+         * The shopify domain for your shop.
+         */
+        'domain' => env('SHOPIFY_DOMAIN', ''),
+
+        /*
+         * The shopify api version.
+         */
+        'api_version' => env('SHOPIFY_API_VERSION', '2021-01'),
+
+    ],
+
+    'webhooks' => [
+
+        /*
+         * The webhook secret provider to use.
+         */
+        'secret_provider' => \Signifly\Shopify\Webhooks\ConfigSecretProvider::class,
+
+        /*
+         * The shopify webhook secret.
+         */
+        'secret' => env('SHOPIFY_WEBHOOK_SECRET'),
+
+    ],
+
+    'exceptions' => [
+
+        /*
+         * Whether to include the validation errors in the exception message.
+         */
+        'include_validation_errors' => false,
+
+    ],
+];
