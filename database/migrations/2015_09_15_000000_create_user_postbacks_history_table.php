f534b1379496a3fdc69fd6ba6ed2070006dd3b64
GGM <tevarjohnson@gmail.com>
2022-07-23 04:55:42 +0000

    Add new file

---
+++ database/migrations/2015_09_15_000000_create_user_postbacks_history_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostbacksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_postbacks_history', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('postback_id');
+            $table->string('subid1');
+            $table->string('subid2');
+            $table->string('subid3');
+            $table->string('subid4');
+            $table->string('subid5');
+            $table->string('url_loaded');
+            $table->string('url_response');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('user_postbacks_history');
+    }
+}
