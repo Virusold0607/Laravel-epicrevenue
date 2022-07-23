8cad6b3036fb80e408edc173ed988770debdca8b
GGM <tevarjohnson@gmail.com>
2022-07-23 04:49:19 +0000

    Add new file

---
+++ database/migrations/2015_09_15_000000_create_user_postbacks_rules_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostbacksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_postbacks_rules', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('name');
+            $table->string('url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('user_postbacks_rules');
+    }
+}
