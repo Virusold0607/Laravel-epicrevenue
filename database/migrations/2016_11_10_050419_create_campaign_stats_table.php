68c3bb74e06e7c4c5410a5d6d8fa913cceed9b3a
GGM <tevarjohnson@gmail.com>
2022-07-24 00:30:52 +0000

    Update 2016_11_10_050419_create_campaign_stats_table.php

--- database/migrations/2016_11_10_050419_create_campaign_stats_table.php
+++ database/migrations/2016_11_10_050419_create_campaign_stats_table.php
@@ -16,10 +16,12 @@ class CreateCampaignStatsTable extends Migration
         Schema::create('campaign_stats', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('campaign_id')->index();
+            $table->date('date');
             $table->integer('clicks');
-            $table->integer('unique_clicks');
             $table->integer('leads');
-            $table->integer('profit');
+            $table->decimal('cr', 4);
+            $table->decimal('revenue', 4);
+            $table->decimal('profit', 4);
             $table->timestamps();
         });
     }
