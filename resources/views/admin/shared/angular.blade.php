e25441e886e0a42a64a636096986222d438e7731
GGM <tevarjohnson@gmail.com>
2022-07-24 00:54:42 +0000

    Update angular.blade.php

--- resources/views/admin/shared/angular.blade.php
+++ resources/views/admin/shared/angular.blade.php
@@ -17,14 +17,14 @@
     <!-- CSS Implementing Plugins -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.0/font/bootstrap-icons.css">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.1.0/ui/trumbowyg.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta3/dist/css/bootstrap-select.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/css/select2.min.css" />
+    
     <!-- CSS Front Template -->
     <link rel="stylesheet" href="{{ asset('assets/css/core.css') }}" data-hs-appearance="default"as="style">
     <link rel="stylesheet" href="{{ asset('assets/css/admin/app.css') }}" data-hs-appearance="default"as="style">
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.1.0/ui/trumbowyg.css" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta3/dist/css/bootstrap-select.min.css" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/css/select2.min.css" />
     @yield('styles')
     <!--<link rel="stylesheet" href="{{ url('/assets/css/admin.css') }}">-->
 </head>
