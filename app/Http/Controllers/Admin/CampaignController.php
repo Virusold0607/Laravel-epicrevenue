b9230293e0c676a24b988af51be5e0337b3a15bc
GGM <tevarjohnson@gmail.com>
2022-07-24 01:20:18 +0000

    Update CampaignController.php

--- app/Http/Controllers/Admin/CampaignController.php
+++ app/Http/Controllers/Admin/CampaignController.php
@@ -291,7 +291,7 @@ class CampaignController extends Controller
             'rate'           => 'required|max:1000000|min:0.1',
             'network_id'     => 'required',
             'network_rate'   => 'required|max:1000000|min:0.1',
-            'network_rate_type'   => 'required',
+            //'network_rate_type'   => 'required',
             'countries'      => 'required|array',
             'feature_image'                       => 'mimes:jpeg,jpg,png',
         ]);
@@ -443,7 +443,7 @@ class CampaignController extends Controller
             'rate'           => 'required|max:1000000|min:0.1',
             'network_id'     => 'required',
             'network_rate'   => 'required|max:1000000|min:0.1',
-            'network_rate_type'   => 'required',
+            //'network_rate_type'   => 'required',
             'countries'      => 'required|array',
             'feature_image'                       => 'mimes:jpeg,jpg,png',
         ]);
