c081fbc6343744df14998bce6bf38fdaa7cd19b7
GGM <tevarjohnson@gmail.com>
2022-07-24 00:25:51 +0000

    Update checkpoint.blade.php

--- resources/views/auth/checkpoint.blade.php
+++ resources/views/auth/checkpoint.blade.php
@@ -1,17 +1,17 @@
 @extends('shared.layout')
 
 @section('body')
-    <div class="hero hero-txt">
-        <div class="container">
-            <h1 class="hero-heading">Account Inaccessible!</h1>
-        </div>
-    </div>
-
-    <div class="clearfix"></div>
-
+<div class="hero hero-auth py-6">
     <div class="container">
-        <div class="container-fluid">
-            <div class="alert alert-danger">{!! $middleware_error !!}</div>
+        <div class="col-lg-4 col-md-4 col-12 text-center mx-auto">
+            <h1 class="fw-800">Account Inaccessible</h1>
+            <p>Please see the reason below.</p>
         </div>
     </div>
+</div><!-- End .hero -->
+
+
+<div class="container py-6">
+    <div class="alert alert-danger">{!! $middleware_error !!}</div>
+</div>
 @endsection
