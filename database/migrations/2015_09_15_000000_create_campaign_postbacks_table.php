37b089e3757d1d746d855459f808f7d258c63aba
GGM <tevarjohnson@gmail.com>
2022-07-24 00:01:07 +0000

    Update 2015_09_15_000000_create_campaign_postbacks_table.php

--- database/migrations/2015_09_15_000000_create_campaign_postbacks_table.php
+++ database/migrations/2015_09_15_000000_create_campaign_postbacks_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCampaignRatesTable extends Migration
+class CreateCampaignPostbacksTable extends Migration
 {
     /**
      * Run the migrations.
