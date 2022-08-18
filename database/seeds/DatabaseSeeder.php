0d554bf6a452d39463ddb465e97d417af2b3fde7
GGM <tevarjohnson@gmail.com>
2022-08-18 19:30:56 +0000

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,15 +1,34 @@
 <?php
 
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call([
+            CampaignsCategoriesTableSeeder::class,
+            PostbacksTableSeeder::class,
+            CountrySeeder::class,
+        ]);
+    }
+
+/*
+
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+
     public function run()
     {
         Model::unguard();
@@ -21,3 +40,4 @@ class DatabaseSeeder extends Seeder
         Model::reguard();
     }
 }
+*/
