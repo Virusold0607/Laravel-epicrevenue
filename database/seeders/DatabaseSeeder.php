c6c0baec6dc96924968cc45e3ae3075026db977f
GGM <tevarjohnson@gmail.com>
2022-09-20 16:13:13 +0000

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            CampaignsCategoriesTableSeeder::class,
-            PostbacksTableSeeder::class,
+            //CampaignsCategoriesTableSeeder::class,
+            //PostbacksTableSeeder::class,
             CountrySeeder::class,
         ]);
     }
