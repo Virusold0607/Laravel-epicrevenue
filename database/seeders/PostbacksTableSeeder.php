0fbf34ba973ccbfec18a68131353de3d5cef2205
GGM <tevarjohnson@gmail.com>
2022-09-20 16:31:31 +0000

    Update PostbacksTableSeeder.php

--- database/seeders/PostbacksTableSeeder.php
+++ database/seeders/PostbacksTableSeeder.php
@@ -15,20 +15,10 @@ class PostbacksTableSeeder extends Seeder
      */
     public function run()
     {
-        $query = "INSERT INTO `campaigns_categories` (`id`, `name`, `created_at`, `updated_at`) VALUES
-        (1, 'Adult', '2022-09-13 18:42:37', '2022-09-13 18:42:37'),
-        (2, 'Android Apps', '2022-09-20 16:22:58', NULL),
-        (3, 'Downloads', '2022-09-20 16:22:58', NULL),
-        (4, 'Email Submits', '2022-09-20 16:22:58', NULL),
-        (5, 'Fitness / Health', '2022-09-20 16:22:59', NULL),
-        (6, 'iOS Apps', '2022-09-20 16:22:59', NULL),
-        (7, 'Mobile Apps', '2022-09-20 16:22:59', NULL),
-        (8, 'Products', '2022-09-20 16:22:59', NULL),
-        (9, 'Registration', '2022-09-20 16:22:59', NULL),
-        (10, 'Travel', '2022-09-20 16:22:59', NULL),
-        (11, 'Trials', '2022-09-20 16:22:59', NULL),
-        (12, 'Sweepstakes/Giveaways', '2022-09-20 16:23:00', NULL),
-        (13, 'Dating', '2022-09-20 16:23:00', NULL);";
+        $query = "INSERT INTO `postbacks` (`id`, `name`, `ips`, `ch_slot`, `reverse_var`, `reverse_key`, `veri_slot`, `created_at`, `updated_at`) VALUES
+(1, 'Maxbounty', NULL, 's1', NULL, NULL, '1e4a3d92d8de85c89d378a0448e6115c', '2022-09-20 16:30:08', NULL),
+(2, 'Adscend', NULL, 'sub1', 'sts', 2, 'fc016c5c4514594dd8ebaaae27476ed1', '2022-09-20 16:30:08', NULL),
+(3, 'Adwork', NULL, 'sid', 'status', 2, 'bec04f3a1c2a1ab4eadf14115c8ff4d2', '2022-09-20 16:30:08', NULL);";
         DB::insert($query);
     }
 }
