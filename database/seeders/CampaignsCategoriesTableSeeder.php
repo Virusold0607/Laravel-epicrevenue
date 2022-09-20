ebb199f0a626e24d723729e73a2659b461492ddb
GGM <tevarjohnson@gmail.com>
2022-09-20 16:24:23 +0000

    Update CampaignsCategoriesTableSeeder.php

--- database/seeders/CampaignsCategoriesTableSeeder.php
+++ database/seeders/CampaignsCategoriesTableSeeder.php
@@ -15,67 +15,20 @@ class CampaignsCategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-
-        //DB::insert($query);
-    }
-}
-
-<?php
-
-//use Illuminate\Database\Seeder;
-//use App\Models\CampaignsCategory;
-//use App\Models\PromotionCategory;
-
-class CampaignsCategoriesTableSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-
-        $cc = CampaignsCategory::firstOrNew(['name' => 'Adult']);
-        $cc->save();
-
-        $cc = CampaignsCategory::firstOrNew(['name' => 'Android Apps']);
-        $cc->save();
-
-        $cc = CampaignsCategory::firstOrNew(['name' => 'Downloads']);
-        $cc->save();
-
-        $cc = CampaignsCategory::firstOrNew(['name' => 'Email Submits']);
-        $cc->save();
-
-        $cc = CampaignsCategory::firstOrNew(['name' => 'Fitness / Health']);
-        $cc->save();
-
-        $cc = CampaignsCategory::firstOrNew(['name' => 'iOS Apps']);
-        $cc->save();
-
-        $cc = CampaignsCategory::firstOrNew(['name' => 'Mobile Apps']);
-        $cc->save();
-
-        $cc = CampaignsCategory::firstOrNew(['name' => 'Products']);
-        $cc->save();
-
-        $cc = CampaignsCategory::firstOrNew(['name' => 'Registration']);
-        $cc->save();
-
-        $cc = CampaignsCategory::firstOrNew(['name' => 'Travel']);
-        $cc->save();
-
-        $cc = CampaignsCategory::firstOrNew(['name' => 'Trials']);
-        $cc->save();
-
-        $p = PromotionCategory::firstOrNew(['name' => 'Sweepstakes/Giveaways']);
-        $p->save();
-
-        $p = PromotionCategory::firstOrNew(['name' => 'Hacks & Tricks']);
-        $p->save();
-
-        $p = PromotionCategory::firstOrNew(['name' => 'Dating']);
-        $p->save();
+        $query = "INSERT INTO `campaigns_categories` (`id`, `name`, `created_at`, `updated_at`) VALUES
+        (1, 'Adult', '2022-09-13 18:42:37', '2022-09-13 18:42:37'),
+        (2, 'Android Apps', '2022-09-20 16:22:58', NULL),
+        (3, 'Downloads', '2022-09-20 16:22:58', NULL),
+        (4, 'Email Submits', '2022-09-20 16:22:58', NULL),
+        (5, 'Fitness / Health', '2022-09-20 16:22:59', NULL),
+        (6, 'iOS Apps', '2022-09-20 16:22:59', NULL),
+        (7, 'Mobile Apps', '2022-09-20 16:22:59', NULL),
+        (8, 'Products', '2022-09-20 16:22:59', NULL),
+        (9, 'Registration', '2022-09-20 16:22:59', NULL),
+        (10, 'Travel', '2022-09-20 16:22:59', NULL),
+        (11, 'Trials', '2022-09-20 16:22:59', NULL),
+        (12, 'Sweepstakes/Giveaways', '2022-09-20 16:23:00', NULL),
+        (13, 'Dating', '2022-09-20 16:23:00', NULL);";
+        DB::insert($query);
     }
 }
